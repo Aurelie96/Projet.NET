0a56c5ab83cc75c615a997a1f96755d1fc695b8f
Aurélie CHASSAGNE <aurelie.chassagne@viacesi.fr>
2018-12-17 10:12:36 +0100

    ajout des modification du master

--- View/Connexion.cs
+++ View/Connexion.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using Projet.NET.Controleur;
+using Projet.NET.View;
 
 namespace Projet.NET
 {
-    public partial class Form1 : Form
+    public partial class Connexion : Form
     {
-        public Form1()
+        public Connexion()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnnule_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnConnexion_Click(object sender, EventArgs e)
+        {
+            bool test = false;
+            test = UsersDAO.Connexion(txtLogin.Text, txtMotDePasse.Text);
+            if(test == true)
+            {
+                Accueil accueil = new Accueil();
+                accueil.Hide();
+                accueil.Show();
+            }
+            else
+            {
+                MessageBox.Show("Votre mot de passe ou login est incorrect");
+            }
+        }
+
+        private void linkInscription_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Inscription inscrire = new Inscription();
+            inscrire.Hide();
+            inscrire.Show();
         }
     }
 }
